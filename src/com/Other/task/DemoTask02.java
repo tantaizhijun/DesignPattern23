95e63281648fca6f86c41c97c6c71bf9816a4562
tantaizhijun <2228048772@qq.com>
2019-11-07 20:23:18 +0800

    任务调度demo

---
+++ src/com/Other/task/DemoTask02.java
@@ -0,0 +1,30 @@
+package com.Other.task;
+
+
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.support.CronTrigger;
+
+/**
+ * 测试定时任务
+ *     基于接口实现 SchedulingConfigurer
+ */
+
+/*
+@Component
+@Configuration          //用于标记配置类，兼备Component的效果。
+@EnableScheduling       //开启定时任务
+*/
+public class DemoTask02 implements SchedulingConfigurer {
+
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addTriggerTask(
+                () -> System.out.println("定时任务"),
+                triggerContext -> {
+                    return new CronTrigger("0/5 * * * * ?").nextExecutionTime(triggerContext);
+                }
+        );
+    }
+}
