7250ce5db83d54bedcd357aa3fec0bf050b64f84
tanzj <tanzj@asiainfo.com>
2020-08-28 14:39:00 +0800

    快排

--- src/main/java/com/test/Other/z_sort/BubbleSort.java
+++ src/main/java/com/test/Other/z_sort/BubbleSort.java
@@ -11,16 +11,25 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{3,2,7,1,9,4,6,8};
+        int[] arr = new int[]{3,9,4,6,8,3,100};
         System.out.println(Arrays.toString(arr));
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
 
+
+        System.out.println("====");
+        int[] b = new int[]{3,9,4,6,8,3,100};
+        selectSort(b);
+        System.out.println(Arrays.toString(b));
+
     }
 
 
+    /**
+     * 冒泡排序
+     * @param arr
+     */
     public static void bubbleSort(int[] arr) {
-
         for (int i = 0; i < arr.length-1; i++) {            //外层循环控制排序趟数
             for (int j = 0; j < arr.length-i-1; j++) {      //内层循环控制每一趟排序多少次
                 if (arr[j] > arr[j+1]) {                //正序，如果倒序显示用"<"
@@ -30,6 +39,29 @@ public class BubbleSort {
         }
     }
 
+
+    /**
+     * 选择排序
+     * @param arr
+     */
+    public static void selectSort(int[] arr){
+        for (int i= 0;i<arr.length-1;i++){
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[i]>arr[j]) {
+                    changePosition(arr,i,j);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    /**
+     * 换位
+     */
     public static void changePosition(int[] arr,int i,int j) {
         int temp = arr[i];
         arr[i] = arr[j];
--- src/main/java/com/test/Other/z_sort/QuickSort.java
+++ src/main/java/com/test/Other/z_sort/QuickSort.java
@@ -4,18 +4,49 @@ import java.util.Arrays;
 
 /**
  * 快速排序
+ * 快速排序的本质就是把基准数大的都放在基准数的右边,把比基准数小的放在基准数的左边,这样就找到了该数据在数组中的正确位置.
+ *   以后采用递归的方式分别对前半部分和后半部分排序，当前半部分和后半部分均有序时该数组就自然有序了。
  */
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = new int[]{4,2,6,2,5,23,64,9};
         System.out.println(Arrays.toString(arr));
-        quickSort(arr);
+        quickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
 
     }
 
-    public static void quickSort(int[] arr) {
+    public static void quickSort(int[] arr,int low,int high) {
+        if(low < high) {
+            int index = getIndex(arr,low,high);
+            quickSort(arr,low,index-1);
+            quickSort(arr,index+1,high);
+        }
+    }
 
+    //寻找基准数据的索引
+    private static int getIndex(int[] arr,int low,int high){
+        //以low位置为 基准数据
+        int tmp = arr[low];
+        while(low < high){
+            // 当队尾的元素大于等于基准数据时,向前挪动high指针
+            while (low < high && arr[high] >= tmp) {
+                high--;
+            }
+            // 如果队尾元素小于tmp了,需要将其赋值给low
+            arr[low] = arr[high];
+
+            // 当队首元素小于等于tmp时,向前挪动low指针
+            while (low < high && arr[low] <= tmp) {
+                low++;
+            }
+            // 当队首元素大于tmp时,需要将其赋值给high
+            arr[high] = arr[low];
+        }
+        // 跳出循环时low和high相等,此时的low或high就是tmp的正确索引位置
+        // 由原理部分可以很清楚的知道low位置的值并不是tmp,所以需要将tmp赋值给arr[low]
+        arr[low] = tmp;
+        return low; // 返回tmp的正确位置
     }
 
 }
